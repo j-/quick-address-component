7232bd31235b12db3592e5e906b9e03edc121d4e
Jamie Hoeks <hoeksj@anz.com>
2019-10-30 11:38:51 +1100

    Add documentation to query state

--- src/query-state.ts
+++ src/query-state.ts
@@ -1,6 +1,11 @@
+/** Tracks where a query is in its request/response life cycle. */
 export enum QueryState {
+  /** This query has been initialized but a request has not yet been made. */
   DEFAULT = 'DEFAULT',
+  /** A request has been made for this query but it has not yet resolved. */
   PENDING = 'PENDING',
+  /** The query request was responded to successfully. */
   SUCCESS = 'SUCCESS',
+  /** An error occurred after the request was made. */
   FAILURE = 'FAILURE',
 }
