007ded55f443f69a82c2c3eeb72edaa1d5e5879d
Jamie Hoeks <hoeksj@anz.com>
2019-10-30 11:26:43 +1100

    Remove pending state from query status component

--- src/components/QueryStatus.tsx
+++ src/components/QueryStatus.tsx
@@ -14,7 +14,6 @@ const QueryStatus: React.FC<Props> = ({ queryId }) => {
   const hasResults = useSelector<RootReducerState, boolean>((state) => hasQueryResultsForQueryId(state, queryId));
   return (
     <div className="QueryStatus mt-3 mb-3">
-      {query.state === QueryState.PENDING && <>Loading&hellip;</>}
       {query.state === QueryState.FAILURE && <>Error: {query.error} <RetryQueryButton queryId={query.id} /></>}
       {query.state === QueryState.SUCCESS && !hasResults && <>No suggestions</>}
     </div>
