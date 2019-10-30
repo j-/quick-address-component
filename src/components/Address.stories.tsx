529ac5b2956d34aa39ad5be2939741f4b63a3bad
Jamie Hoeks <hoeksj@anz.com>
2019-10-30 11:30:32 +1100

    Remove app component and index

---
+++ src/components/Address.stories.tsx
@@ -0,0 +1,8 @@
+import * as React from 'react';
+import { storiesOf } from '@storybook/react';
+import Address from './Address';
+
+storiesOf('Address', module)
+  .add('Example', () => (
+    <Address />
+  ));
