fd480172a5110af296a7d2acacf2fac5c2f456f0
Jamie Hoeks <hoeksj@anz.com>
2019-11-04 10:50:25 +1100

    Document the "use ID" hook

--- src/use-id.ts
+++ src/use-id.ts
@@ -1,5 +1,16 @@
 import * as React from 'react';
 
+/**
+ * Returns a function which, when called with a 'prefix' string, will return a
+ * ID that matches the pattern (prefix + "-" + random string). Used to ensure
+ * IDs are identical within an instance of a component but not between
+ * instances.
+ *
+ * @example
+ *
+ * const id = useId();
+ * const fieldId = id('value-input'); // => "value-input-4gpk5sf84t6"
+ */
 export const useID = () => {
   const rand = Math.random().toString(36).substring(2);
   const ref = React.useRef(rand);
