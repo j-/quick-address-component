08db6b257b075e1c54b07741abcd2d953703cd6b
Jamie Hoeks <hoeksj@anz.com>
2019-11-04 10:46:12 +1100

    Document the "use window listener" hook

--- src/use-window-listener.ts
+++ src/use-window-listener.ts
@@ -1,3 +1,13 @@
+/**
+ * Attach an event listener to the `window` global. Will remove the listener on
+ * cleanup.
+ *
+ * @example
+ *
+ * useWindowListener('keydown', (e) => {
+ *   console.log('Pressed key ' + e.key);
+ * });
+ */
 export const useWindowListener = <K extends keyof WindowEventMap>(type: K, listener: (this: Window, ev: WindowEventMap[K]) => any) => {
   window.addEventListener(type, listener);
   return () => window.removeEventListener(type, listener);
