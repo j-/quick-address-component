235ab1fb1ef6333067c87fa65811f2d3c7ddd0e7
Jamie Hoeks <hoeksj@anz.com>
2019-11-04 11:26:20 +1100

    Document "normalize query" function

--- src/normalize-query.ts
+++ src/normalize-query.ts
@@ -1,5 +1,15 @@
 import { QueryEntity } from './entities';
 
+/**
+ * Normalizes a query term (what the user typed in) to its canonical form.
+ * Will allow two queries to be considered the same despite differences in
+ * whitespace, upper/lower case, special characters etc.
+ *
+ * @example
+ *
+ * normalizeQuery('833  collins st'); // => "833 collins st"
+ * normalizeQuery('833 Collins St.'); // => "833 collins st"
+ */
 export const normalizeQuery = (query: string): QueryEntity['id'] => (
   query
     // Remove insignificant characters
