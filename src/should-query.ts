9322c6b5cb5bdf3b06aae04d53d7a077a036e4e5
Jamie Hoeks <hoeksj@anz.com>
2019-11-04 11:21:42 +1100

    Refactor "should query" function
    Also adds documentation

--- src/should-query.ts
+++ src/should-query.ts
@@ -1,5 +1,25 @@
 import { MIN_SEARCH_QUERY_LENGTH } from './constants';
 
-export const shouldQuery = (term: string) => (
-  term.replace(/[^\w]/g, '').length >= MIN_SEARCH_QUERY_LENGTH
+/**
+ * Determines if a query should be made to the address service. Checks that
+ * there is enough meaningful information in the query term to justify making a
+ * request.
+ *
+ * Returns `true` if the number of letters meets or exceeds the minimum query
+ * length requirement.
+ *
+ * @param term Query term. Does not need to be normalized. Can be exactly what
+ * the user entered.
+ *
+ * @param [min] Optional minimum for query length. Used when being explicit
+ * about length (e.g. in unit tests). Otherwise omit this to use the global
+ * constant value.
+ *
+ * @example
+ *
+ * shouldQuery('833 co', 3); // => false
+ * shouldQuery('833 collins', 3); // => true
+ */
+export const shouldQuery = (term: string, min: number = MIN_SEARCH_QUERY_LENGTH) => (
+  (term.match(/[A-Za-z]/g) || []).length >= min
 );
