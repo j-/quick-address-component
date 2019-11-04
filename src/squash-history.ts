9c187b7780eacce5624caf9552c431953181ff8e
Jamie Hoeks <hoeksj@anz.com>
2019-11-04 11:03:04 +1100

    Document the "squash history" utility

--- src/squash-history.ts
+++ src/squash-history.ts
@@ -1,6 +1,21 @@
 import { Results, QueryEntity, EntityMap } from './entities';
 import { QueryState } from './query-state';
 
+/**
+ * Takes an array of strings, each representing a query identifier, as well as
+ * a map of query entities, and returns a copy of the array but with all
+ * resolved queries (except the most recent one) removed.
+ *
+ * Query history is used to determine which query results to show. Only results
+ * for the most recent resolved query are shown. Once a more recent query is
+ * resolved, older queries can be discarded. All pending queries are kept.
+ *
+ * @param history Array of query IDs in reverse chronological order (newest to
+ * oldest)
+ *
+ * @param entities Entity map containing all the queries and their state
+ * indexed by ID
+ */
 export const squashHistory = (history: Results<QueryEntity>, entities: EntityMap<QueryEntity>): Results<QueryEntity> => {
   const result: Results<QueryEntity> = [];
   let foundSuccess = false;
