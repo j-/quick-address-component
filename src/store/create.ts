8a24ed37cb82f879b24e26807f5a1a523aa5e109
Jamie Hoeks <hoeksj@anz.com>
2019-11-07 14:49:10 +1100

    Use Redux devtools extension in dev only

--- src/store/create.ts
+++ src/store/create.ts
@@ -1,6 +1,6 @@
 import { createStore, applyMiddleware } from 'redux';
 import rootReducer, { RootReducerState } from '.';
-import { composeWithDevTools } from 'redux-devtools-extension';
+import { composeWithDevTools } from 'redux-devtools-extension/developmentOnly';
 import { actionCreators } from './actions';
 import thunk, { ThunkMiddleware } from 'redux-thunk';
 import { logicMiddleware } from './logic';
